3c9e015d86845f95404ccfabd4f3a54a4c980ab2
salamanderskies <0technocraticaenigma0@gmail.com>
2026-07-29 17:02:25 -0400

    yupp

--- Content.Server/StationEvents/Components/VentClogRuleComponent.cs
+++ Content.Server/StationEvents/Components/VentClogRuleComponent.cs
@@ -15,7 +15,8 @@ public sealed partial class VentClogRuleComponent : Component
     [DataField]
     public IReadOnlyList<ProtoId<ReagentPrototype>> SafeishVentChemicals = new ProtoId<ReagentPrototype>[]
     {
-        "Water", "Blood", "Slime", "SpaceDrugs", "SpaceCleaner", "Nutriment", "Sugar", "SpaceLube", "Ephedrine", "Ale", "Beer", "SpaceGlue"
+        // Aurora's Song - Remove ephedrine
+        "Water", "Blood", "Slime", "SpaceDrugs", "SpaceCleaner", "Nutriment", "Sugar", "SpaceLube", "Ale", "Beer", "SpaceGlue"
     };
 
     /// <summary>
